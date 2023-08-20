724ce4122fa4b54b3e4edb1179177e9d9a83965d
harris <harriskwong120898@gmail.com>
2023-08-19 21:16:51 -0400

    Implemented Upcoming controller and view

---
+++ EventFlow/EventFlow/Controllers/UpcomingController.cs
@@ -0,0 +1,43 @@
+﻿using EventFlow.Data;
+using EventFlow.Models;
+using EventFlow.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventFlow.Controllers
+{
+    public class UpcomingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UpcomingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Upcoming> ObjUpcomingList = await _context.Upcomings.ToListAsync();
+            
+            var UpcomingDto = new List<UpcomingDTO>();
+
+            foreach(var Upcoming in ObjUpcomingList)
+            {
+                UpcomingDto.Add(new UpcomingDTO()
+                {
+                    Id = Upcoming.Id,
+                    Title = Upcoming.Title,
+                    Priority = Upcoming.Priority,
+                    Date = Upcoming.Date,
+                    Type = Upcoming.Type,
+                    UserId = Upcoming.UserId
+                });
+            }
+            
+            return View(UpcomingDto);
+        
+            //return View(ObjUpcomingList);
+        }
+    }
+}
---
+++ EventFlow/EventFlow/Models/DTO/UpcomingDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventFlow.Models.DTO
+{
+    public class UpcomingDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Priority { get; set; }
+        public string Type { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+    }
+}
