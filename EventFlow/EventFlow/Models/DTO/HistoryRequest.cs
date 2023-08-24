a01445b551100e7631d5f833f967c6557d1c43b1
harris <harriskwong120898@gmail.com>
2023-08-24 11:20:28 -0400

    Updated histories index view

---
+++ EventFlow/EventFlow/Models/DTO/HistoryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EventFlow.Models.DTO
+{
+    public class HistoryRequest
+    {
+        public string Title { get; set; }
+        public string Completion { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
