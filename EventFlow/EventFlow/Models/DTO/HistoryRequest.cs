50d3d709c4becf3ee827166dfe4a46dfe5ee981c
harris <harriskwong120898@gmail.com>
2023-08-25 11:01:14 -0400

    Revert "Updated histories index view"
    This reverts commit a01445b551100e7631d5f833f967c6557d1c43b1.

--- EventFlow/EventFlow/Models/DTO/HistoryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventFlow.Models.DTO
-{
-    public class HistoryRequest
-    {
-        public string Title { get; set; }
-        public string Completion { get; set; }
-        public DateTime Date { get; set; }
-    }
-}
